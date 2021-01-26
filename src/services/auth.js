22f37d41f494f3808caba1b798655f0e882b04aa
Matheus Barbosa Lopes <matheus_barbosa5@hotmail.com>
2021-01-26 17:41:16 -0300

    Ajustes nos metodos de auth

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,12 +1,12 @@
-const token = process.env.REACT_APP_TOKEN_KEY
-export const isAuthenticated = () => localStorage.getItem(token);
-export const getToken = () => localStorage.getItem(token);
+const tokenName = process.env.REACT_APP_TOKEN_KEY;
+export const isAuthenticated = () => localStorage.getItem(tokenName);
+export const getToken = () => localStorage.getItem(tokenName);
 
-export const signin = async token => {
-  await localStorage.setItem(token, token);
-  window.location = "/";
+export const signIn = async (token) => {
+  await localStorage.setItem(tokenName, token);
+  window.location = "/home";
 };
 export const logout = async () => {
-  await localStorage.removeItem(token);
-  window.location = "/signin";
+  await localStorage.removeItem(tokenName);
+  window.location = "/";
 };
