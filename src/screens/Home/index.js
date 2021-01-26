7de1eb840b4186c4987d934d41b44f9116b96fbb
Matheus Barbosa Lopes <matheus_barbosa5@hotmail.com>
2021-01-26 17:44:01 -0300

    Iniciado ajustes no layout do HOME para o uso da lista e do MAPS do google

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
 
-// import { Container } from './styles';
+import { Grid } from "@material-ui/core";
+
+import Map from "~/components/Map";
+
+import useStyles from "./styles";
 
 const Home = () => {
-  return <h1>Home</h1>;
-}
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.root} spacing={2}>
+      <Grid item xs={6}>
+        aaa
+      </Grid>
+      <Grid item xs={6}>
+        <Map />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Home;
