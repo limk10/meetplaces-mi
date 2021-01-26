135d79a9f8cbaba3b94747387012e714d58ceb5b
Matheus Barbosa Lopes <matheus_barbosa5@hotmail.com>
2021-01-26 17:44:42 -0300

    Screen que terá os componentes de registro e login

--- src/screens/Auth/index.js
+++ src/screens/Auth/index.js
@@ -1,9 +1,72 @@
-import React from "react";
- 
-// import { Container } from './styles';
+import React, { useEffect, useState } from "react";
+import { TextField, Button, Typography, Link, Fade } from "@material-ui/core";
+
+import useStyles from "./styles";
+
+import meetIllustration from "~/assets/images/g10.svg";
+import { isAuthenticated } from "~/services/auth";
+
+import { useHistory } from "react-router-dom";
+
+import SignInForm from "./SignIn";
+import SignUpForm from "./SignUp";
 
 const Login = () => {
-  return <h1>Login</h1>;
-}
+  const [wantRegister, setWantRegister] = useState(false);
+  const classes = useStyles();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (isAuthenticated()) history.push("/home");
+  }, []);
+
+  return (
+    <div
+      className={`${classes.backgroundContainer} ${classes.centeredContainer}`}
+    >
+      <div className={`${classes.loginContainer} ${classes.centeredContainer}`}>
+        <div className={classes.artSection}>
+          <img
+            loading="lazy"
+            className={classes.imageIllustration}
+            src={meetIllustration}
+          />
+        </div>
+        <div className={classes.formSection}>
+          {!wantRegister && (
+            <>
+              <SignInForm />
+              <Typography
+                className={classes.registerText}
+                variant="body1"
+                gutterBottom
+              >
+                Não possui conta?{" "}
+                <Link href="#" onClick={() => setWantRegister(!wantRegister)}>
+                  Registre-se
+                </Link>
+              </Typography>
+            </>
+          )}
+          {wantRegister && (
+            <>
+              <SignUpForm />
+              <Typography
+                className={classes.registerText}
+                variant="body1"
+                gutterBottom
+              >
+                Já possui uma conta?{" "}
+                <Link href="#" onClick={() => setWantRegister(!wantRegister)}>
+                  Log in
+                </Link>
+              </Typography>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
---
+++ src/screens/Auth/styles.js
@@ -0,0 +1,69 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  centeredContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+  backgroundContainer: {
+    background: "#6CCB94",
+    backdropFilter: "blur(5px)",
+  },
+  loginContainer: {
+    background: "#ffff",
+    minHeight: "100vh",
+    width: "70vw",
+    margin: "0 auto",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  imageIllustration: {
+    maxWidth: "80%",
+    height: "auto",
+    margin: "0 auto",
+  },
+  artSection: {
+    flex: 1,
+    borderRight: "1px solid #3C8E65",
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+  formSection: {
+    flex: 1,
+  },
+  btnEnter: {
+    marginTop: theme.spacing(1.5),
+  },
+  registerText: {
+    marginTop: 20,
+  },
+  formContainer: {
+    width: "85%",
+    margin: "0 auto",
+  },
+  loginText: {
+    fontWeight: "bold",
+    textAlign: "left",
+  },
+  subtitleLoginText: {
+    textAlign: "left",
+    color: "#6c757d",
+    marginBottom: 30,
+  },
+  textField: {
+    marginBottom: theme.spacing(2),
+  },
+  buttonProgress: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -5,
+    marginLeft: -12,
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: "relative",
+  },
+}));
+
+export default useStyles;
