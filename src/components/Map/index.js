26937cff27d63e26f0af42b46b951ea3e9f902f0
Matheus Barbosa Lopes <matheus_barbosa5@hotmail.com>
2021-01-27 12:41:44 -0300

    Ajusta no mapa para pegar local atual do usuario e armazenar nos tore

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,26 +1,75 @@
-import React, { memo } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Typography, LinearProgress } from "@material-ui/core";
 
 import useStyles from "./styles";
 
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import GoogleMapReact from "google-map-react";
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
+import actionsLocations from "~/actions/locations";
 
 function MapWrapper() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+
+  const currentLocation = useSelector(
+    (state) => state.reducerLocations.addCurrentLocation || {}
+  );
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  const getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      setLoading(true);
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position?.coords?.latitude,
+            lng: position?.coords?.longitude,
+          };
+
+          dispatch(actionsLocations.addCurrentLocation({ ...pos }));
+          setLoading(false);
+        },
+        (e) => {
+          setLoading(false);
+          console.log("erro", e);
+        }
+      );
+    }
+  };
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyAXXuqigRDn8HXWo97siE0vRqhlKqQPo70">
-      <GoogleMap
-        mapContainerStyle={classes.mapStyles}
-        center={center}
-        zoom={10}
-      ></GoogleMap>
-    </LoadScript>
+    <div className={classes.mapStyles}>
+      {loading && (
+        <>
+          <Typography
+            style={{ textAlign: "center" }}
+            variant="overline"
+            display="block"
+            gutterBottom
+          >
+            Estamos carregando seu mapa... aguarde um instante :)
+          </Typography>
+          <LinearProgress style={{ width: "50%", margin: "0 auto" }} />
+        </>
+      )}
+      {!loading && !!Object.keys(currentLocation).length && (
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: process.env.REACT_APP_API_KEY,
+            language: "pt-BR",
+          }}
+          defaultCenter={currentLocation}
+          defaultZoom={14}
+        ></GoogleMapReact>
+      )}
+    </div>
   );
 }
 
-export default memo(MapWrapper);
+export default MapWrapper;
--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -2,8 +2,8 @@ import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   mapStyles: {
-    width: "400px",
-    height: "400px",
+    width: "100%",
+    height: "100%",
   },
 }));
 
