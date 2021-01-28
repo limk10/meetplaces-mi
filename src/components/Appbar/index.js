a82789cae6c55e17a3ea38f553c52aac258364b0
Matheus Barbosa Lopes <matheus_barbosa5@hotmail.com>
2021-01-28 00:01:17 -0300

    Ajustado nave'

--- src/components/Appbar/index.js
+++ src/components/Appbar/index.js
@@ -1,24 +1,36 @@
 import React from "react";
-
-import useStyles from "./styles";
-
+import { useHistory } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
 
 import MenuPerfil from "./components/MenuPerfil";
+import useStyles from "./styles";
 
 const Navbar = () => {
   const classes = useStyles();
+  const history = useHistory();
+
+  const handleRoute = (route) => {
+    history.push(route);
+  };
 
   return (
     <div className={classes.root}>
       <AppBar className={classes.appBar} position="static">
         <Container style={{ paddingLeft: 0, paddingRight: 0 }} maxWidth="xl">
           <Toolbar>
-            <Typography className={classes.logo} variant="h6">
+            <Typography
+              onClick={() => handleRoute("/home")}
+              className={classes.logo}
+              variant="h6"
+            >
               &gt; Meet Places
             </Typography>
             <div className={classes.divider}>
-              <Typography className={classes.menu} variant="h6">
+              <Typography
+                onClick={() => handleRoute("/home")}
+                className={classes.menu}
+                variant="h6"
+              >
                 Home
               </Typography>
             </div>
